4183bbfb3c70f5d0c7058469bf1d2bb445ae56af
Vincent Moucadeau <generation2geek@gmail.com>
2021-07-31 23:30:49 +0200

    Test commit

--- lua/cleantickets/sh_ctlanguage.lua
+++ lua/cleantickets/sh_ctlanguage.lua
@@ -1,31 +1,53 @@
 local Language_EN = {
-    -- [TICKETS STATUS] --
+    -- [PANEL]
+    -- TABS
+    PANEL_TAB_SENDTICKET = "Nouveau ticket",
+    PANEL_TAB_MYTICKETS = "My Ticket",
+    PANEL_TAB_ADMIN = "Admin Panel",
+
+    -- [TICKETS]
+    -- TICKETS STATUS --
     TICKETSTATUS_OPEN = "Open",
     TICKETSTATUS_TAKEN = "Taken",
-    TICKETSTATUS_CLOSED = "Closed"
+    TICKETSTATUS_CLOSED = "Closed",
+
+    -- TICKETS POPUP --
+    TICKET_SUBJECT = "Reason:",
+    TICKET_DESCRIPTION = "Message:",
+    TICKET_SELECTEDPLAYER = "Selected player(s):",
+    TICKET_ADMIN = "Admin:",
+
+    -- TICKETS BUTTONS --
+    TICKET_BUT_TAKE = "Take Ticket",
+    TICKET_BUT_GOTO = "Goto",
+    TICKET_BUT_TELEPORT = "Teleport",
+    TICKET_BUT_SPECTATE = "Spectate",
+    TICKET_BUT_FREEZE = "Freeze",
+    TICKET_BUT_MORE = "More",
+    TICKET_BUT_CLOSE = "Close",
+    TICKET_BUT_DELETE = "Delete"
 }
 
 local Language_FR = {
     -- [PANEL]
-        -- TABS
+    -- TABS
     PANEL_TAB_SENDTICKET = "Nouveau ticket",
     PANEL_TAB_MYTICKETS = "Mes tickets",
     PANEL_TAB_ADMIN = "Administration",
 
-
     -- [TICKETS]
-        -- TICKETS STATUS --
+    -- TICKETS STATUS --
     TICKETSTATUS_OPEN = "Ouvert",
     TICKETSTATUS_TAKEN = "En cours",
     TICKETSTATUS_CLOSED = "Fermé",
 
-        -- TICKETS POPUP --
+    -- TICKETS POPUP --
     TICKET_SUBJECT = "Raison :",
     TICKET_DESCRIPTION = "Message :",
     TICKET_SELECTEDPLAYER = "Joueurs sélectionnés :",
     TICKET_ADMIN = "Admin :",
 
-        -- TICKETS BUTTONS --
+    -- TICKETS BUTTONS --
     TICKET_BUT_TAKE = "Prendre le ticket",
     TICKET_BUT_GOTO = "Goto",
     TICKET_BUT_TELEPORT = "Téléporter ici",
@@ -36,7 +58,5 @@ local Language_FR = {
     TICKET_BUT_DELETE = "Supprimer"
 }
 
-
 CleanTickets.Lang = Language_FR
-
 
