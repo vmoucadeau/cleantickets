c3a56f0072dbb5e60359ac2a4afb1d9699cab27f
Vincent <generation2geek@gmail.com>
2022-07-12 23:20:14 +0200

    config modifs

--- lua/cleantickets/sh_ctconfig.lua
+++ lua/cleantickets/sh_ctconfig.lua
@@ -7,19 +7,6 @@ CleanTickets.Config.ErrorSound = "buttons/button10.wav"
 CleanTickets.Config.TicketSound = "buttons/lightswitch2.wav"
 CleanTickets.Config.dateformat = "%d/%m/%Y" -- see https://www.lua.org/pil/22.1.html
 CleanTickets.Config.timeformat = "%H:%M"
-
-CleanTickets.Config.SubjectsList = {
-    "Troll",
-    "Bug(s)",
-    "Offense",
-    "RolePlay Problem"
-}
-CleanTickets.Config.ChatCommand = "!ticket" -- Command to send a ticket (and access to the admin panel)
-CleanTickets.Config.AutoCloseTimer = 10 -- Auto close time (in seconds)
-CleanTickets.Config.TicketNotifs = true -- Send a notification to the ticket's sender when admin click on "goto", "teleport"...
-CleanTickets.Config.HideOnTake = false -- Close the ticket popup when an admin take it
-
-
 CleanTickets.Config.TicketPos = {
     x = 25,
     y = 25
@@ -28,7 +15,16 @@ CleanTickets.Config.TicketBackground = Color(40, 40, 40, 255)
 CleanTickets.Config.TicketOutline = Color(70, 70, 70, 255)
 CleanTickets.Config.MaxTicketsOnScreen = nil -- If nil then this value will be based on the height of the screen
 
--- [GUI]
+CleanTickets.Config.SubjectsList = {
+    "Troll",
+    "Bug(s)",
+    "Offense",
+    "RolePlay Problem"
+}
+CleanTickets.Config.ChatCommand = "!ticket" -- Command to send a ticket (and access to the admin panel)
+CleanTickets.Config.TicketNotifs = true -- Send a notification to the ticket's sender when admin click on "goto", "teleport"...
+CleanTickets.Config.HideOnTake = true -- Close the ticket popup when an admin take it
+
 
 -- Clean Tickets Panel
 CleanTickets.Config.PrimaryColor = Color(189, 189, 189)
@@ -47,11 +43,11 @@ CleanTickets.Config.MaxDescCharacters = 200 -- max characters that can be send t
 -- Tickets Status Colors
 CleanTickets.Config.OpenColor = Color(40, 200, 81)
 CleanTickets.Config.TakenColor = Color(239, 142, 7)
-CleanTickets.Config.ClosedColor = Color(255, 0, 0)
+CleanTickets.Config.ClosedColor = Color(252, 42, 5)
 
 -- [SERVER] 
 
-CleanTickets.Config.Debug = true -- Admins can send tickets
+CleanTickets.Config.Debug = false -- Admins can send tickets
 CleanTickets.Config.SendTicketDelay = 60 -- Time to wait (in seconds) to send a new ticket (spam protection)
 CleanTickets.Config.AdminGroups = { -- ULX Groups that will receive the tickets
     "superadmin",
